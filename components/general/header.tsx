00de917d84bc97fefe88803d6b123a5210266a16
ispasdani <ispasdani14@gmail.com>
2025-05-28 00:04:54 +0200

    1:08:33

--- components/general/header.tsx
+++ components/general/header.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <nav>nav inside header</nav>
+    </div>
+  );
 };
 
 export default Header;
