81c0364d651ab40671f8b5091e4c47a551dd83e9
ispasdani <ispasdani14@gmail.com>
2025-06-05 21:15:03 +0200

    dot background and framer motion babyy

--- components/general/dotBackground/dotBackground.tsx
+++ components/general/dotBackground/dotBackground.tsx
@@ -7,7 +7,7 @@ export function DotBackgroundDemo() {
       <div
         className={cn(
           "absolute inset-0",
-          "[background-size:20px_20px]",
+          "[background-size:15px_15px]",
           "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
           "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]"
         )}
